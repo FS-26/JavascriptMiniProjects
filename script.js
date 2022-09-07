7f87ef72066d037598df840a6893054c95cce964
Hippolyte EZIAN <polarusdev@gmail.com>
2022-09-07 17:06:38 +0100

    Update method.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let icon = document.querySelector("i");
 let paragraph = document.querySelector("#content");
 
 icon.addEventListener("click", function () {
-  paragraph.classList.remove("hide");
+  paragraph.classList.toggle("hide");
   if (paragraph.classList.contains("hide")) {
     icon.classList.replace("fa-minus", "fa-plus");
   } else {
