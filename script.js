1a53bfc477e48a2e3e49bba653056c0288c5bfdc
Hippolyte EZIAN <polarusdev@gmail.com>
2022-09-08 16:43:15 +0100

    Co-authored-by: Salmasaidi <salma594@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let title = document.querySelector("h2");
 let icon = document.querySelector("i");
 let paragraph = document.querySelector("#content");
 
-icon.addEventListener("click", function () {
+title.addEventListener("click", function () {
   paragraph.classList.toggle("hide");
   if (paragraph.classList.contains("hide")) {
     icon.classList.replace("fa-minus", "fa-plus");
