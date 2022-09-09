7a0c4c3dcda42ac0bf26dec33f99dbb94b6632b3
Hippolyte EZIAN <polarusdev@gmail.com>
2022-09-09 10:28:53 +0100

    BMi Ok

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let button = document.querySelector("button");
 button.addEventListener("click", function () {
   let poids = Number(inputs[1].value);
   let taille = Number(inputs[0].value) / 100;
-  let bmi = (poids / (taille * taille)).toFixed(2);
+  let bmi = (poids / (taille * taille)).toPrecision(4);
   document.querySelector("#resultat").textContent = bmi;
   inputs[0].value = "";
   inputs[1].value = "";
