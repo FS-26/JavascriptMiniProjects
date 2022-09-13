a96367dff0c8d4cb93343ff6219bf3e6d51cd50b
Hippolyte EZIAN <polarusdev@gmail.com>
2022-09-13 16:11:07 +0100

    valid inputs

--- script.js
+++ script.js
@@ -3,16 +3,34 @@ let form = document.querySelector("form");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let inputs = form.elements;
-  let invMess = document.querySelector(".invalid_message");
-  if (inputs["nom"].value.length == 0) {
-    inputs["nom"].className = "is_invalid";
-    invMess.textContent = "Le champ Nom est Obligatoire";
-  } else {
-    inputs["nom"].className = "is_valid";
-    invMess.textContent = "";
-    // recuperation de la valeur saisi qui est valide
-    const nom = inputs["nom"].value;
-    console.log(nom);
-  }
-  const email = form.elements["email"].value;
+  checkField(inputs["nom"]);
+  checkField(inputs["email"]);
+  checkEmail(inputs["email"]);
 });
+
+function checkEmail(field) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9]{3,}\.[a-zA-Z]{2,}$/;
+  if (regex.test(field.value)) {
+    field.className = "is_valid";
+    if (document.querySelector("input+.invalid_message")) {
+      document.querySelector("input+.invalid_message").remove();
+    }
+  } else {
+    field.className = "is_invalid";
+    if (!document.querySelector("input+.invalid_message")) {
+      let div = document.createElement("div");
+      div.textContent = "Please Enter a valid Email";
+      div.className = "invalid_message";
+      field.after(div);
+    }
+  }
+}
+
+function checkField(field) {
+  if (field.value.length == 0) {
+    field.className = "is_invalid";
+  } else {
+    field.className = "is_valid";
+    return field.value;
+  }
+}
